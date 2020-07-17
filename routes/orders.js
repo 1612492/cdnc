d649b87d84d113bb9715fe60db0a31d834288809
HuyDoan198 <49835032+HuyDoan198@users.noreply.github.com>
2020-07-17 23:55:45 +0700

    Change 2

--- routes/orders.js
+++ routes/orders.js
@@ -5,9 +5,7 @@ const OrderController = require("../controllers/OrderController");
 // const config = require('../config/index');
 // const paramValidation = require('../config/param-validation');
 
-router.get("/", function (req, res, next) {
-    return res.json({ message: "Oder OK" });
-  });
+router.get("/", OrderController.list);
 router.post("/create", OrderController.placeOrder);
 router.get("/:orderId", OrderController.get);
 router.param("orderId", OrderController.load);
